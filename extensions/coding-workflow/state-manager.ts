e1c5e272a9f28220c71aa0e6a7a70a42a8cd94dd
ZZzzswszzZZ <zhushanwen321@hotmail.com>
2026-05-17 11:15:28 +0800

    fix: remove unused imports from state-manager

--- extensions/coding-workflow/state-manager.ts
+++ extensions/coding-workflow/state-manager.ts
@@ -1,7 +1,6 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import type { WorkflowState, PhaseId, PhaseTransitionState } from "./types.js";
-import { createInitialState } from "./types.js";
+import type { WorkflowState, PhaseId } from "./types.js";
 import { getPhaseConfig, getStageList } from "./stages.js";
 
 const STATE_FILENAME = "workflow-state.json";
